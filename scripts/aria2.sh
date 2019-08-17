42a313f0f4fa35c395be1aed7ef90923db1f7268
anshengme <ianshengme@gmail.com>
2019-08-17 15:39:41 +0800

    Update aria2.sh

--- scripts/aria2.sh
+++ scripts/aria2.sh
@@ -1,3 +1,14 @@
-yum install epel-release -y
-yum install aria2 -y
+aria2_session="/etc/aria2/aria2.session" 
+lockFile="/opt/lock" 
+
+if [ ! -f "$aria2_session" ]; then 
+    touch $aria2_session
+fi
+
+if [ ! -f "$lockFile" ]; then 
+    yum install epel-release -y
+    yum install aria2 -y
+    touch $lockFile
+fi 
+
 aria2c --conf-path=/etc/aria2/aria2.conf
